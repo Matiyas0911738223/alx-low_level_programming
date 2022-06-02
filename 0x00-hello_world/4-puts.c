1e37f333d574be4bb34ea108512c1191e420e6ca
Matiyas0911738223 <Mati@123456>
2022-06-02 12:17:33 -0700

    my first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+  *main - print the given sentence
+  *
+  *Return: always 0
+  */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
