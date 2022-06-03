3ce128afac9b4fd4c6e6b60555d2d1e0c06bf71b
Matiyas0911738223 <Mati@123456>
2022-06-03 12:45:44 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print lowercase alphabet excluding e and q
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+	{
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
