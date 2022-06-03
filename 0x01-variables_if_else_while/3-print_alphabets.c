e58bf03b898e061ef9dd170cfa042f21d32fa33c
Matiyas0911738223 <Mati@123456>
2022-06-03 12:42:43 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print lower and upper alphabet on the same line
+ * Return: 0
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		putchar(ch);
+	for (ch = 'A'; ch <= 'Z'; ++ch)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
+
