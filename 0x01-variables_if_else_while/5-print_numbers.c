517816d2103a482c9d9fc90ebed4a31190c3d2a3
Matiyas0911738223 <Mati@123456>
2022-06-03 12:47:55 -0700

    my first commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print numbers 0 to 9 on one line
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+	printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
+
