a245c4d7e88f13b210bdaaca54fc41afbda144cb
Matiyas0911738223 <Mati@123456>
2022-06-13 12:01:23 -0700

    arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - function which swap value of a to b
+ * @a: temporary value of a to be changed with b
+ * @b: temporary value of b to be changed with a
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+
