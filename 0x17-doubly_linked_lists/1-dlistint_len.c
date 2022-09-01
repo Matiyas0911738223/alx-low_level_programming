c078c5a1ac692793d6422c152081e093ba373e8c
Matiyas0911738223 <Mati@123456>
2022-09-01 05:55:16 -0700

    1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include "lists.h"
 
 /**
@@ -21,4 +22,52 @@ size_t dlistint_len(const dlistint_t *h)
 		len++;
 	}
 	return (len);
+}#!/bin/bash
+#include "lists.h"
+
+/**
+ *  * dlistint_len - a function that returns the number
+ *   * of elements in a linked dlistint_t list
+ *    * @h: head node
+ *     *
+ *      * Return: lenght of elements
+ *       */
+size_t dlistint_len(const dlistint_t *h)
+{
+		register int len = 0;
+
+			if (h == NULL)
+			{
+						printf("%s", "Empty list");
+			}
+				while (h != NULL)
+				{
+							h = h->next;
+									len++;
+				}
+					return (len);
+}#!/bin/bash
+#include "lists.h"
+
+/**
+ *  * dlistint_len - a function that returns the number
+ *   * of elements in a linked dlistint_t list
+ *    * @h: head node
+ *     *
+ *      * Return: lenght of elements
+ *       */
+size_t dlistint_len(const dlistint_t *h)
+{
+		register int len = 0;
+
+			if (h == NULL)
+			{
+						printf("%s", "Empty list");
+			}
+				while (h != NULL)
+				{
+							h = h->next;
+									len++;
+				}
+					return (len);
 }
